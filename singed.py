01163d17632acd3c3d4f8a4423d86938328d3858
jasonau4 <jasonau9999@gmail.com>
2025-01-11 15:09:43 -0800

    deleted old files

--- singed.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from PIL import Image
-
-vander1 = Image.open('vander.jpg')
-vander2 = mpimg.imread('vander.jpg')
-
-vander1.show()
