8f4c1fb8a41a2a62b3d43145ca39aefdd166df75
jasonau4 <jasonau9999@gmail.com>
2025-01-11 09:23:07 -0800

    hi

---
+++ sevika.py
@@ -0,0 +1,12 @@
+import random
+
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(10):
+                for e in range(10):
+                    for f in range(10):
+                        print("sevika", a, b, c, d, e, f)
+                        i = random.randint(0,999999)
+                        if i == 100000*a+10000*b+1000*c+100*d+10*e+f: 
+                            print("you won ", a,b,c,d,e,f, "prancoins")
