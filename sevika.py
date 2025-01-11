87ed598db732912ff8c4b501ede4cc5735546350
jasonau4 <jasonau9999@gmail.com>
2025-01-11 09:24:03 -0800

    prancoins

--- sevika.py
+++ sevika.py
@@ -1,12 +1,17 @@
 import random
 
-for a in range(10):
-    for b in range(10):
-        for c in range(10):
-            for d in range(10):
-                for e in range(10):
-                    for f in range(10):
-                        print("sevika", a, b, c, d, e, f)
-                        i = random.randint(0,999999)
-                        if i == 100000*a+10000*b+1000*c+100*d+10*e+f: 
-                            print("you won ", a,b,c,d,e,f, "prancoins")
+def sevika():
+    for a in range(10):
+        for b in range(10):
+            for c in range(10):
+                for d in range(10):
+                    for e in range(10):
+                        for f in range(10):
+                            print("sevika", a, b, c, d, e, f)
+                            i = random.randint(0,999999)
+                            if i == 100000*a+10000*b+1000*c+100*d+10*e+f: 
+                                return("you won ", a,b,c,d,e,f, "prancoins")
+    return("you won 0 prancoins :(")
+
+sevika()
+
